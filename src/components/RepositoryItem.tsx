603493aa54aacbb9029f1b7269c566be00e8d100
Werner Gehrke Junior <werner.junior@outlook.com>
2021-03-09 20:50:13 -0300

    Adicionando Typescript ao projeto

---
+++ src/components/RepositoryItem.tsx
@@ -0,0 +1,20 @@
+interface RepositoryItemProps {
+  repository: {
+    name: string;
+    description: string;
+    html_url: string;
+  }
+}
+
+export function RepositoryItem( { repository } : RepositoryItemProps ){
+  return(
+    <li>
+    <strong>{repository.name ?? 'Default'}</strong>
+    <p>{repository.description ?? 'Default'}</p>
+
+    <a href={repository.html_url ?? ''}>
+      Acessar repositório
+    </a>
+  </li>
+  )
+}
--- src/components/RepositoryList.tsx
+++ src/components/RepositoryList.tsx
@@ -3,8 +3,14 @@ import { RepositoryItem } from "./RepositoryItem";
 import '../styles/repositories.scss';
 import { useEffect, useState } from "react";
 
+interface Repository{
+    name: string;
+    description: string;
+    html_url: string;
+}
+
 export function RepositoryList(){
-  const [repositories, setRepositories] = useState([]);
+  const [repositories, setRepositories] = useState<Repository[]>([]);
 
   useEffect(() => {
     fetch('https://api.github.com/orgs/rocketseat/repos')
@@ -17,8 +23,9 @@ export function RepositoryList(){
       <h1>Lista de repositórios</h1>
 
       <ul>
-        {repositories.map(repository => <RepositoryItem key={repository.name} repository={repository} />)}
-        
+        {repositories.map(repository => (
+          <RepositoryItem key={repository.name} repository={repository} />
+        ))}        
       </ul>
     </section>
   )
